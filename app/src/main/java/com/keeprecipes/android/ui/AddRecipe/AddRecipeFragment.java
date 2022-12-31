43face2d77952da3a84bd25260fcde98d8ae1c8f
RoneyThomas <roneythomas6@gmail.com>
2022-12-31 10:56:22 -0500

    Add click listener for save action in AddRecipeFragment

--- app/src/main/java/com/keeprecipes/android/ui/AddRecipe/AddRecipeFragment.java
+++ app/src/main/java/com/keeprecipes/android/ui/AddRecipe/AddRecipeFragment.java
@@ -1,6 +1,9 @@
 package com.keeprecipes.android.ui.AddRecipe;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -10,6 +13,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -31,7 +37,11 @@ public class AddRecipeFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentAddRecipeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        // AddRecipeFragment has it's toolbar,
+        // here we are setting title, back arrow and the menu for toolbar
         binding.toolbar.setTitle("Add Recipe");
+        binding.toolbar.inflateMenu(R.menu.add_recipe_menu);
+        binding.toolbar.setNavigationIcon(R.drawable.ic_outline_arrow_back_24);
         return root;
     }
 
@@ -39,8 +49,16 @@ public class AddRecipeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(AddRecipeViewModel.class);
-        binding.toolbar.inflateMenu(R.menu.add_recipe_menu);
-        binding.toolbar.setNavigationIcon(R.drawable.ic_outline_arrow_back_24);
+        // To go back to previous fragment
         binding.toolbar.setNavigationOnClickListener(view1 -> requireActivity().onBackPressed());
+        // menu item click listener
+        binding.toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_save) {// Navigate to settings screen
+                // Go back to previous fragment after saving
+                requireActivity().onBackPressed();
+                return true;
+            }
+            return false;
+        });
     }
 }
