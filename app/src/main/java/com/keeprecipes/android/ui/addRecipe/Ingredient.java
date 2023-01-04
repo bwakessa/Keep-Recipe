f73efa642dc74f529c3c7d9317d9a6c0d34d741f
RoneyThomas <roneythomas6@gmail.com>
2023-01-04 04:05:45 -0500

    Added recyclerview for ingredients in add recipe fragment.

---
+++ app/src/main/java/com/keeprecipes/android/ui/addRecipe/Ingredient.java
@@ -0,0 +1,11 @@
+package com.keeprecipes.android.ui.addRecipe;
+
+public class Ingredient {
+    public String name;
+    public int size;
+
+    public Ingredient(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+}
