6d72a7975d1222551fa666d8e8969d579b8cf952
RoneyThomas <roneythomas6@gmail.com>
2023-01-08 19:43:30 -0500

    Fixed test errors.

--- app/src/androidTest/java/com/keeprecipes/android/SimpleRDBTest.java
+++ app/src/androidTest/java/com/keeprecipes/android/SimpleRDBTest.java
@@ -1,35 +1,36 @@
-package com.keeprecipes.android.test;
+package com.keeprecipes.android;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import android.content.Context;
 
 import androidx.lifecycle.LiveData;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.keeprecipes.android.dataLayer.AppDatabase;
 import com.keeprecipes.android.dataLayer.dao.RecipeDao;
 import com.keeprecipes.android.dataLayer.entities.Recipe;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 
+@RunWith(AndroidJUnit4.class)
 public class SimpleRDBTest {
     private static RecipeDao recipeDao;
     private static AppDatabase db;
 
-    @BeforeAll
-    public static void createDb() {
+    @Before
+    public void createDb() {
         Context context = ApplicationProvider.getApplicationContext();
         db = AppDatabase.getDatabase(context);
         recipeDao = db.recipeDao();
     }
 
-    @AfterAll
-    public static void closeDb() throws IOException {
+    @After
+    public void closeDb() throws IOException {
         db.close();
     }
 
