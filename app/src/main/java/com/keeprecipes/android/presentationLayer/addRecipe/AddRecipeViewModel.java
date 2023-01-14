e3e22462a75e68aa09151f3495454be9987cb383
RoneyThomas <roneythomas6@gmail.com>
2023-01-14 07:57:18 -0500

    Added livedata objects for photos

--- app/src/main/java/com/keeprecipes/android/presentationLayer/addRecipe/AddRecipeViewModel.java
+++ app/src/main/java/com/keeprecipes/android/presentationLayer/addRecipe/AddRecipeViewModel.java
@@ -1,15 +1,32 @@
 package com.keeprecipes.android.presentationLayer.addRecipe;
 
+import android.app.Application;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.keeprecipes.android.dataLayer.entities.Recipe;
+import com.keeprecipes.android.dataLayer.repository.RecipeRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AddRecipeViewModel extends ViewModel {
+public class AddRecipeViewModel extends AndroidViewModel {
 
-    public MutableLiveData<RecipeDTO> recipe = new MutableLiveData<>();
-    public MutableLiveData<List<IngredientDTO>> ingredients = new MutableLiveData<>();
+    public MutableLiveData<RecipeDTO> recipe = new MutableLiveData<>(new RecipeDTO());
+    public MutableLiveData<List<IngredientDTO>> ingredients = new MutableLiveData<>(new ArrayList<>());
+
+    public MutableLiveData<List<PhotoDTO>> photos = new MutableLiveData<>(new ArrayList<>());
+
+    private RecipeRepository recipeRepository;
+
+    public AddRecipeViewModel(@NonNull Application application) {
+        super(application);
+        recipeRepository = new RecipeRepository(application);
+    }
 
     public void addIngredient() {
         List<IngredientDTO> ingredientList = ingredients.getValue() == null ? new ArrayList<>() : new ArrayList<>(ingredients.getValue());
@@ -24,5 +41,25 @@ public class AddRecipeViewModel extends ViewModel {
             ingredientList.remove(ingredientList.size() - 1);
             ingredients.postValue(ingredientList);
         }
+    }
+
+    public void addPhotos(Uri uri) {
+        List<PhotoDTO> photoList = photos.getValue() == null ? new ArrayList<>() : new ArrayList<>(photos.getValue());
+        PhotoDTO photo = new PhotoDTO(photoList.size(), uri);
+        photoList.add(photo);
+        photos.postValue(photoList);
+    }
+
+    public void removePhotos(int position) {
+        List<PhotoDTO> photoList = photos.getValue() == null ? new ArrayList<>() : new ArrayList<>(photos.getValue());
+        if (photoList.size() > position) {
+            photoList.remove(position);
+            photos.postValue(photoList);
+        }
+    }
+
+    public void saveRecipe() {
+        Recipe recipeToSave = new Recipe();
+//        recipeRepository.insert(recipe.getValue());
     }
 }
