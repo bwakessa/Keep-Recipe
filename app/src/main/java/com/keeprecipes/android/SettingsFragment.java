b365cd2d18277d88e7f2eaa1f38bcccf2eb4e102
RoneyThomas <roneythomas6@gmail.com>
2023-03-05 10:37:39 -0500

    Add privacy link in settings

--- app/src/main/java/com/keeprecipes/android/SettingsFragment.java
+++ app/src/main/java/com/keeprecipes/android/SettingsFragment.java
@@ -1,21 +1,23 @@
 package com.keeprecipes.android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
-import androidx.databinding.DataBindingUtil;
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.keeprecipes.android.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
@@ -34,10 +36,15 @@ public class SettingsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         NavController navController = Navigation.findNavController(view);
-        AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(navController.getGraph()).build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         Toolbar toolbar = view.findViewById(R.id.toolbar);
-        NavigationUI.setupWithNavController(
-                toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+
+        binding.deleteTextView.setOnClickListener(view1 -> new MaterialAlertDialogBuilder(view1.getContext()).setTitle("Delete all recipes?").setMessage("This will all delete recipes. There is no turning back.")
+                .show());
+
+        binding.privacyTextView.setOnClickListener(view2 -> {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://roney.me/")));
+        });
     }
 }
