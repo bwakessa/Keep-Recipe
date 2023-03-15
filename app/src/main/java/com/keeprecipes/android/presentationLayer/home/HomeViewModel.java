077d4c69bcf5c0d5df30ecf122823b17324ebad3
RoneyThomas <roneythomas6@gmail.com>
2023-03-15 12:16:01 -0400

    Added support for API level 26

--- app/src/main/java/com/keeprecipes/android/presentationLayer/home/HomeViewModel.java
+++ app/src/main/java/com/keeprecipes/android/presentationLayer/home/HomeViewModel.java
@@ -1,6 +1,7 @@
 package com.keeprecipes.android.presentationLayer.home;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -8,22 +9,29 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 
+import com.keeprecipes.android.dataLayer.entities.Collection;
 import com.keeprecipes.android.dataLayer.entities.Recipe;
+import com.keeprecipes.android.dataLayer.repository.CollectionRepository;
 import com.keeprecipes.android.dataLayer.repository.RecipeRepository;
 
 import java.util.List;
 
 public class HomeViewModel extends AndroidViewModel {
+    final String TAG = "AndroidViewModel";
 
     private final LiveData<List<Recipe>> recipe;
-    private final MutableLiveData<Integer> recipeId = new MutableLiveData();
+    private final MutableLiveData<Integer> recipeId;
+    private CollectionRepository collectionRepository;
     private RecipeRepository recipeRepository;
-    public final LiveData<Recipe> selectedRecipe = Transformations.switchMap(recipeId, (recipe) -> recipeRepository.fetchById(recipeId.getValue()));
+    public final LiveData<Recipe> selectedRecipe;
 
     public HomeViewModel(@NonNull Application application) {
         super(application);
         this.recipeRepository = new RecipeRepository(application);
+        this.collectionRepository = new CollectionRepository(application);
         this.recipe = recipeRepository.getAllRecipes();
+        this.recipeId = new MutableLiveData<>();
+        this.selectedRecipe = Transformations.switchMap(recipeId, (recipe) -> recipeRepository.fetchById(recipeId.getValue()));
     }
 
     public LiveData<List<Recipe>> getRecipe() {
@@ -42,11 +50,16 @@ public class HomeViewModel extends AndroidViewModel {
         recipeRepository.deleteAllRecipes();
     }
 
-    public LiveData<List<String>> getAllCuisineCollection() {
-        return recipeRepository.getAllCuisineCollection();
+    public LiveData<List<String>> getAllCollection() {
+        return recipeRepository.getAllCollection();
     }
 
     public LiveData<List<Recipe>> searchRecipe(String query) {
         return recipeRepository.searchRecipe(query);
+    }
+
+    public LiveData<Collection> collectionFetchByName(String query) {
+        Log.d(TAG, "collectionFetchByName: " + query);
+        return collectionRepository.fetchByName(query);
     }
 }
